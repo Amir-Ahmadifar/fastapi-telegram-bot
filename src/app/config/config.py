0fa300bcae4d6a6085e5201edce73aaed52f23c1
Amir-Ahmadifar <amirahmadifaar@gmail.com>
2025-08-29 15:41:20 +0330

    codes updated

--- src/app/config/config.py
+++ src/app/config/config.py
@@ -1,16 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import AnyHttpUrl, SecretStr
+from pydantic import AnyHttpUrl, SecretStr, field_validator
 from typing import List
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=False)
 
     ENV: str = "development"
-    APP_NAME: str = "fastapi-telegram-pro"
+    APP_NAME: str = "keraseh_bot"
     APP_HOST: str = "0.0.0.0"
     APP_PORT: int = 8000
 
-    ALLOWED_ORIGINS: List[AnyHttpUrl] | List[str] = []
+    ALLOWED_ORIGINS: List[AnyHttpUrl] = []
 
     TELEGRAM_BOT_TOKEN: SecretStr
     TELEGRAM_WEBHOOK_URL: AnyHttpUrl
@@ -30,11 +30,11 @@ class Settings(BaseSettings):
 
     LOG_LEVEL: str = "INFO"
 
-    @property
-    def database_url(self) -> str:
-        return (
-            f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD.get_secret_value()}@"
-            f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        )
+    @field_validator("ALLOWED_ORIGINS", mode="before")
+    @classmethod
+    def split_allowed_origins(cls, v):
+        if isinstance(v, str):
+            return [x.strip() for x in v.split(",")]
+        return v
 
 settings = Settings()
