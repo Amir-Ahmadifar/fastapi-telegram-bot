dc46e5c2973bb677757179404d3da7d86c982f5d
Amir-Ahmadifar <amirahmadifaar@gmail.com>
2025-08-29 17:50:48 +0330

    codes updated

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import types
 from aiogram.filters import Command
-from src.app.services.bot_service import dp
+from app.services.bot_service import dp
 
 @dp.message(Command("start"))
 async def start_handler(message: types.Message):
